92630162a59de595e1b551af5d294df3d375a13f
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-12-30 19:49:09 +0100

    secured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {ReservationComponent} from "./components/reservation/reservation.component";
 import {ResourceComponent} from "./components/resource/resource.component";
 import {PersonComponent} from "./components/person/person.component";
+import {AuthGuard} from "./guards/authentication.guard";
 
 const routes: Routes = [
-  {path:"reservation",component:ReservationComponent,data:{roles:["ADMIN"]}},
-  {path:"resource",component:ResourceComponent,data:{roles:["ADMIN"]}},
-  {path:"person",component:PersonComponent,data:{roles:["ADMIN"]}},
+  {path:"reservation",component:ReservationComponent,data:{roles:["ADMIN"]},canActivate:[AuthGuard]},
+  {path:"resource",component:ResourceComponent,data:{roles:["ADMIN"]},canActivate:[AuthGuard]},
+  {path:"person",component:PersonComponent,data:{roles:["ADMIN"]},canActivate:[AuthGuard]},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,23 @@ import { ResourceComponent } from './components/resource/resource.component';
 import {HttpClientModule} from "@angular/common/http";
 import {NgxPaginationModule} from "ngx-pagination";
 import { PersonComponent } from './components/person/person.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'auth_apps',
+        clientId: 'angular_auth'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 @NgModule({
   declarations: [
@@ -20,9 +37,17 @@ import { PersonComponent } from './components/person/person.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
